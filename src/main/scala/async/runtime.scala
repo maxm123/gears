1c959d8c586fa3c09c185664ea923c9ce5cc0fbb
Maximilian Müller <maxm123@techie.com>
2023-09-26 18:08:21 +0200

    mess with continuations

--- src/main/scala/async/runtime.scala
+++ src/main/scala/async/runtime.scala
@@ -1,8 +1,31 @@
 package runtime
 import scala.util.boundary, boundary.Label
 
+import jdk.internal.vm.{Continuation, ContinuationScope}
+
 /** Contains a delimited contination, which can be invoked with `resume` */
 class Suspension[-T, +R]:
   def resume(arg: T): R = ???
 
-def suspend[T, R](body: Suspension[T, R] => R)(using Label[R]): T = ???
+private class MySuspension[T, R] extends Suspension[T, R]:
+  private[runtime] var cont: Continuation = null
+  private[runtime] var res: Option[T] = None
+  private[runtime] var out: Option[R] = None
+
+  override def resume(arg: T): R =
+    res = Some(arg)
+    cont.run()
+    out.get
+
+def suspend[T, R](body: Suspension[T, R] => R)(using Label[R]): T =
+  val scope = new ContinuationScope("scala-runtime")
+
+  val sus = new MySuspension[T, R]
+  val cont: Continuation = new Continuation(scope, () => {
+    sus.out = Some(body(sus))
+    Continuation.`yield`(scope)
+  })
+  sus.cont = cont
+  cont.run()
+
+  sus.res.get
