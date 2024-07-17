69b5ab81cd183b36aaf95bd8af4c156b67ca7a99
Maximilian Müller <m8n.mueller@gmail.com>
2024-07-17 22:33:24 +0200

    Complete MixedStream#run for transformed stream

--- shared/src/main/scala/async/stream/MixedStream.scala
+++ shared/src/main/scala/async/stream/MixedStream.scala
@@ -11,56 +11,82 @@ trait BottomEnd[T[_ <: StreamType.StreamPos]]:
   def storeInput(in: T[StreamIn]): Unit
   def loadOutput(): T[StreamOut]
 
-type TransformIn[T <: StreamType] = StreamType.Applied[TopEnd, T]
-type TransformOut[T <: StreamType] = StreamType.Applied[BottomEnd, T]
-
-// val _ =
-//   summon[TransformOut[StreamType.PushSender[String] **: SEmpty] =:= Tuple1[BottomEnd[StreamType.PushSender[String]]]]
+type TransformTop[T <: StreamType] = StreamType.Applied[TopEnd, T]
+type TransformBot[T <: StreamType] = StreamType.Applied[BottomEnd, T]
 
 trait MixedStream[T <: StreamType]:
   self =>
   def run(in: ForPos[StreamIn, T]): Resource[ForPos[StreamOut, T]]
 
-  def transform[O <: StreamType, S <: TransformIn[T]](f: S => TransformOut[O])(using fac: => S): MixedStream[O] =
+  def transform[O <: StreamType, S <: TransformTop[T]](f: S => TransformBot[O])(using
+      fac: MixedStream.Fac[S]
+  ): MixedStream[O] =
     new MixedStream[O]:
-      def storeInputs[C <: StreamType](out: TransformOut[C], inputs: ForPos[StreamIn, C]): Unit = out match
+      def storeInputs[C <: StreamType](out: TransformBot[C], inputs: ForPos[StreamIn, C]): Unit = out match
         case _: EmptyTuple => ()
-        case outt: *:[BottomEnd[tpe], TransformOut[tailTpe]] =>
+        case outt: *:[BottomEnd[tpe], TransformBot[tailTpe]] =>
           type InTpe = tpe[StreamIn]
-          type TailTpe = StreamType.ForPos[StreamIn, tailTpe]
+          type TailTpe = ForPos[StreamIn, tailTpe]
           inputs match
             case _: EmptyTuple => ()
             case inputt: *:[InTpe, TailTpe] =>
               outt.head.storeInput(inputt.head)
               storeInputs(outt.tail, inputt.tail)
 
-      def getInputs0[C <: StreamType](in: TransformIn[C]): ForPos[StreamIn, C] = in match
-        case _: EmptyTuple => Tuple().asInstanceOf[ForPos[StreamIn, C]]
-        case int: *:[TopEnd[tpe], TransformIn[tailTpe]] =>
-          int.head.getInput() *: getInputs0(int.tail)
-
       type TopTypes[X <: StreamType.StreamPos, Ins <: Tuple] <: Tuple = Ins match
         case EmptyTuple          => EmptyTuple
         case TopEnd[tpe] *: rest => tpe[X] *: TopTypes[X, rest]
-
-      // def x[R] = summon[TopTypes[StreamIn, TransformIn[R]] =:= ForPos[StreamIn, R]] // TODO
 
       def getInputs[Ins <: Tuple](in: Ins): TopTypes[StreamIn, Ins] = in match
         case _: EmptyTuple               => Tuple()
         case rest: (TopEnd[tpe] *: rest) => rest.head.getInput() *: getInputs(rest.tail)
 
+      def setOutputs[C <: StreamType](out: TransformTop[C], outputs: ForPos[StreamOut, C]): Unit = out match
+        case _: EmptyTuple => ()
+        case outt: *:[TopEnd[tpe], TransformTop[tailTpe]] =>
+          type OutTpe = tpe[StreamOut]
+          type TailTpe = ForPos[StreamOut, tailTpe]
+          outputs match
+            case _: EmptyTuple => ()
+            case outputt: *:[OutTpe, TailTpe] =>
+              outt.head.setOutput(outputt.head)
+              setOutputs(outt.tail, outputt.tail)
+
+      type BottomTypes[X <: StreamType.StreamPos, Outs <: Tuple] <: Tuple = Outs match
+        case EmptyTuple             => EmptyTuple
+        case BottomEnd[tpe] *: rest => tpe[X] *: BottomTypes[X, rest]
+
+      def loadOutputs[Outs <: Tuple](in: Outs): BottomTypes[StreamOut, Outs] = in match
+        case _: EmptyTuple                  => Tuple()
+        case rest: (BottomEnd[tpe] *: rest) => rest.head.loadOutput() *: loadOutputs(rest.tail)
+
       override def run(in: ForPos[StreamIn, O]): Resource[ForPos[StreamOut, O]] =
-        val inputs = fac
+        val inputs = fac.generate()
         val outputs = f(inputs)
         storeInputs(outputs, in)
-        val in0: ForPos[StreamIn, T] = ??? // getInputs[TransformIn[T]](inputs)
-        val out0 = self.run(in0)
-        ???
-      // override def transform[O2 <: StreamType, S2 <: TransformIn[O]](f: S2 => TransformOut[O2])(using
-      //     fac: => S2
-      // ): MixedStream[O2] = ???
+
+        val in0: TopTypes[StreamIn, TransformTop[T]] = getInputs[TransformTop[T]](inputs)
+        // lemma: TopTypes[StreamIn, TransformIn[T]] =:= ForPos[StreamIn, T]
+        val in1 = in0.asInstanceOf[ForPos[StreamIn, T]]
+        self
+          .run(in1)
+          .map: out =>
+            setOutputs(inputs, out)
+            val out0: BottomTypes[StreamOut, TransformBot[O]] = loadOutputs(outputs)
+            // lemma: BottomTypes[StreamOut, TransformBot[O]] =:= ForPos[StreamOut[O]]
+            val out1 = out0.asInstanceOf[ForPos[StreamOut, O]]
+            out1
+      end run
+  end transform
+end MixedStream
 
 object MixedStream:
+  opaque type Fac[+T] = () => T
+  extension [T](f: Fac[T]) inline def generate() = f()
+
+  given Fac[EmptyTuple] = () => EmptyTuple
+  given [T, S <: Tuple](using inner: Fac[S]): Fac[TopEnd[PushSender[T]] *: S] = () => ???
+
   extension [T](stream: PullReaderStream[T])
     def toMixed(parallelism: Int): MixedStream[PullReader[T] **: SEmpty] = new MixedStream:
       def run(in: ForPos[StreamIn, PullReader[T] **: SEmpty]): Resource[ForPos[StreamOut, PullReader[T] **: SEmpty]] =
@@ -75,5 +101,5 @@ object MixedStream:
     stream.toMixed(stream.parallelismHint)
 
 extension [T <: StreamType](mixed: MixedStream[T])
-  def addPush(): Unit = ???
+  def prependPush[V](stream: PushSenderStream[V]): MixedStream[PushSender[V] **: T] = ???
   // TODO problem: push stream runs synchronously in alloc, pull stream does not
